84597034935b7706eb5f1ab64c1a584fd3e86251
Luar RaNa <me@luarrana.dev>
2021-04-25 09:18:14 +0000

    Add phx.gen.auth to dependencies

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Caterpie.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:phx_gen_auth, "~> 0.7", only: [:dev], runtime: false},
       {:phoenix, "~> 1.5.8"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
