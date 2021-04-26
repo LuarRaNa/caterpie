4ceebf5dd55a568dd0533f1ddec4e7c54806e399
Luar RaNa <me@luarrana.dev>
2021-04-26 08:22:16 +0000

    Configure alpinejs

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@
 // its own CSS file.
 import "../css/app.css"
 
+import 'alpinejs';
+
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
@@ -18,7 +20,14 @@ import topbar from "topbar"
 import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {_csrf_token: csrfToken},
+  dom: {
+    onBeforeElUpdated(from, to){
+      if(from.__x){ window.Alpine.clone(from.__x, to) }
+    }
+  },
+})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
